bb3eef013304f6d50598d7fe7af41a55a8a4d109
Carolzita Dev <42356399+devcarolzita@users.noreply.github.com>
2025-01-23 20:25:27 -0300

    Add food gallery functionality with filtering options

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,36 @@
 import {foods} from '../data.js';
-console.log(foods)
+const foodGallery = document.querySelector('#food-gallery');
+const foodFilter = document.querySelector('#food-filter');
+
+const createFood = (item) => {
+  const foodDiv = document.createElement('div');
+  foodDiv.classList.add('food-item');
+
+  const img = document.createElement('img');
+  img.src = item.image;
+
+  const titleCard = document.createElement('p');
+  titleCard.textContent = item.name;
+
+  foodDiv.appendChild(img);
+  foodDiv.appendChild(titleCard);
+
+  return foodDiv;
+}
+
+const renderFoods = (itensFoods) => {
+  foodGallery.innerHTML = '';
+  const foodElements = itensFoods.map((item) => createFood(item));
+  foodGallery.append(...foodElements);
+}
+
+const filteredFoods = (filter) => {
+  const resultsFiltered = foods.filter((item) => item.tipo === filter || filter === 'all');
+  renderFoods(resultsFiltered);
+};
+
+foodFilter.addEventListener('change', (event) => {  
+  filteredFoods(event.target.value)
+});
+
+renderFoods(foods);
