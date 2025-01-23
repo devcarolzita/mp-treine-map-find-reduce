0e22041c6244a6190f3851c6b3c2e960633b5764
Carolzita Dev <42356399+devcarolzita@users.noreply.github.com>
2025-01-23 20:26:08 -0300

    Add drink creation functionality and event listener

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,39 @@
 import {drinks} from '../data.js'
+const buttonSelect = document.querySelector('#criarDrink');
+const resultado = document.querySelector('#resultado');
 
-console.log(drinks);
+const createDrinkItem = (item) => {
+  const drinkDiv = document.createElement('div');
+  drinkDiv.classList.add('drink-item');
+
+  const img = document.createElement('img');
+  img.src = item.image;
+
+  const boxTexts = document.createElement('div');
+  boxTexts.classList.add('drink-item-box-texts');
+
+  const title = document.createElement('h3');
+  title.textContent = item.name;
+
+  const description = document.createElement('p');
+  description.textContent = item.description;
+
+  drinkDiv.appendChild(img);
+  boxTexts.appendChild(title);
+  boxTexts.appendChild(description);
+  drinkDiv.appendChild(boxTexts);
+  
+  return drinkDiv;
+}
+
+const findDrinks = () => {
+  resultado.innerHTML = '';
+  const frutaSelect = document.querySelector('#fruta').value;
+  const distilledSelect = document.querySelector('#destilado').value;
+  const resposta = drinks.find((item) => {
+    return item.fruit === frutaSelect && item.distilled === distilledSelect;
+  })
+  resultado.appendChild(createDrinkItem(resposta));
+}
+
+buttonSelect.addEventListener('click', findDrinks)
